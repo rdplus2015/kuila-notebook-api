602b9e9cf55eeb8e84a01648ff8b5feaf81be959
Ridi Otoko <rdplus2015@gmail.com>
2026-04-29 16:25:31 -0400

    Init project ; model added

---
+++ kuilaNotebook/notebook/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Notebook(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # String representation of the obj
+    def __str__(self):
+        return self.title
