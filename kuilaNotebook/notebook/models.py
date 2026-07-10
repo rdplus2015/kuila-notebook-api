ad2e49bab4fb0e727f41a48066122db3e2342245
Ridi Otoko <rdplus2015@gmail.com>
2026-07-10 13:44:25 -0400

    notebook model updated: authentifaction added to the model

--- kuilaNotebook/notebook/models.py
+++ kuilaNotebook/notebook/models.py
@@ -2,17 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-
 class Category(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)  # update retrieve the user from the setting later
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Notebook(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='notes')
+    author =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    is_pinned =  models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
